d4c4a5c4f36e42eccff0d44521a31c0e82c2da14
Kovacs Erik <kovaroberik@gmail.com>
2018-12-03 21:56:03 +0200

    added methods from Vintze's implementation

--- library/utils.py
+++ library/utils.py
@@ -1,5 +1,7 @@
+import numpy as np
+
 '''
-Utility functions go here. All are static
+    Utility methods go here. All are static
 '''
 
 class Utils:
@@ -22,5 +24,25 @@ class Utils:
         file.write(contents)
         file.close()
 
+    # Replace NA (not available, not applicable) or NaN (not a number) cells
+    # with column (variable) mean
+    # for a pandas DataFrame
+    @staticmethod
+    def replace_NA(X):
+        avgs = np.nanmean(X, axis=0)
+        pos = np.where(np.isnan(X))
+        print(pos[:])
+        X[pos] = avgs[pos[1]]
+        return X
+
+    # No offence but let's speak the Queen's
+    # Standardise the column (variable) values
+    # for a pandas DataFrame
+    @staticmethod
+    def standardise(X):
+        avgs = np.mean(X, axis=0)
+        stds = np.std(X, axis=0)
+        Xstd = (X - avgs) / stds
+        return Xstd
 
 
